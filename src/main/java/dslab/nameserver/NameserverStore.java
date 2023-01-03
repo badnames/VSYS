2445641a40d0175318772e7c2150e8edcc8d6f06
Moritz Schalk <e52005233@student.tuwien.ac.at>
2023-01-03 17:18:35 +0100

    Add Store for nameserver data.

---
+++ src/main/java/dslab/nameserver/NameserverStore.java
@@ -0,0 +1,44 @@
+package dslab.nameserver;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class NameserverStore {
+    // Singleton
+    private NameserverStore() {}
+
+    private static class InstanceHolder {
+        private static final NameserverStore INSTANCE = new NameserverStore();
+    }
+
+    public static synchronized NameserverStore getInstance() {
+        return NameserverStore.InstanceHolder.INSTANCE;
+    }
+
+    private ConcurrentHashMap<String, String> mailboxes = new ConcurrentHashMap<>();
+    private ConcurrentHashMap<String, INameserverRemote> subZones = new ConcurrentHashMap<>();
+
+    public void addSubZone(String domain, INameserverRemote remote) {
+        subZones.put(domain, remote);
+    }
+
+    public void deleteSubZone(String domain){
+        subZones.remove(domain);
+    }
+
+    public INameserverRemote getSubZone(String domain) {
+        return subZones.get(domain);
+    }
+
+
+    public void addMailbox(String domain, String remote) {
+        mailboxes.put(domain, remote);
+    }
+
+    public void deleteMailbox(String domain){
+        mailboxes.remove(domain);
+    }
+
+    public String getMailbox(String domain) {
+        return mailboxes.get(domain);
+    }
+}
