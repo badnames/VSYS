13044e052e3446f5672d2b24650167e64e78eceb
Moritz Schalk <e52005233@student.tuwien.ac.at>
2023-01-04 16:36:21 +0100

    Remove no longer needed code from nameserver.

--- src/main/java/dslab/nameserver/Nameserver.java
+++ src/main/java/dslab/nameserver/Nameserver.java
@@ -14,6 +14,9 @@ import dslab.util.handler.DispatchListener;
 //A nameserver can communicate with the nameservers on the next lower level
 public class Nameserver implements INameserver {
 
+    private final Config config;
+    private final Shell shell = new Shell();
+
     /**
      * Creates a new server instance.
      *
@@ -22,44 +25,24 @@ public class Nameserver implements INameserver {
      * @param in the input stream to read console input from
      * @param out the output stream to write console output to
      */
-
-    private String componentID;
-    private InputStream in;
-    private PrintStream out;
-    private ArrayList<NameserverDomain> children;  //TODO: put children here
-
-    private final Shell shell;
-    private final Config config;
-
-    private final DispatchListener register;
-    private final DispatchListener listener;
-
-    private String domain;
-
-
     public Nameserver(String componentId, Config config, InputStream in, PrintStream out) throws IOException,
             AlreadyBoundException,
             NotBoundException,
             AlreadyRegisteredException,
             InvalidDomainException {
-        // TODO
-        this.componentID=componentId;
         this.config=config;
-        this.in=in;
-        this.out=out;
 
-        port = config.getInt("registry.port");
-        host = config.getString("registry.host");
-        root_id =  config.getString("root_id");
-        domain = config.getString("domain");
+
+        int port = config.getInt("registry.port");
+        String host = config.getString("registry.host");
+        String root_id = config.getString("root_id");
+        String domain = config.getString("domain");
 
         if (domain == null){
             //root
             LocateRegistry.createRegistry(port).bind(root_id, new NameserverRemote(shell.out()));
         } else {
             //zone nameserver
-            //LocateRegistry.getRegistry(String host,int port), and 2) locating the object using
-            //Registry.lookup(String name), using the name given in the root id property.
             var rootRegistry = LocateRegistry.getRegistry(host, port);
             INameserverRemote rootNameServerRemote = (INameserverRemote) rootRegistry.lookup(root_id);
 
@@ -78,19 +61,7 @@ public class Nameserver implements INameserver {
     //Prints out each known nameserver (zones) in alphabetical order,
     // from the perspective of this nameserver
     public void nameservers() {
-        // TODO: do it for all nameservers (lookup)
-        if (!children.isEmpty()) {
-            String[] names = new String[children.size()];
-            for (int i = 0; i < children.size(); i++) {
-                names[i] = children.get(i).getDomain();
-            }
 
-            Arrays.sort(names);
-
-            for (int i = 0; i < names.length; i++) {
-                shell.out().println(i + ". " + names[i]);
-            }
-        }
     }
 
     @Override
@@ -98,27 +69,6 @@ public class Nameserver implements INameserver {
     //Prints out some information about each stored mailbox server address, containing mail DOMAIN and
     //ADRESSES (IP:port), arranged by the domain in alphabetical order.
     public void addresses() {
-        // TODO: alphabetical
-        if (!children.isEmpty()){
-            String[] names = new String[children.size()];
-            for (int i = 0; i < children.size(); i++) {
-                names[i] = children.get(i).getDomain();
-            }
-
-            //alphabetical sort
-            Arrays.sort(names);
-
-            for (int i = 0; i < children.size(); i++) {
-                //looking for the namestore after the alphabetical sort to extract IP and PORT
-                NameserverDomain nameStore;
-                for (int j = 0; j < children.size(); j++) {
-                    nameStore=children.get(j);
-                    if (nameStore.getDomain().equals(names[i])){
-                        shell.out().println(i+". "+ names[i]+" "+ children.get(j).getIp()+":"+ children.get(j).getPort());
-                    }
-                }
-            }
-        }
 
 
     }
@@ -129,19 +79,6 @@ public class Nameserver implements INameserver {
     public void shutdown() {
         throw new StopShellException();
     }
-
-    public ArrayList<NameserverDomain>  getChildren(){
-        return children;
-    }
-
-    public void addChildren(NameserverDomain store){
-        children.add(store);
-    }
-
-    public void deleteChildren(NameserverDomain store){
-        children.remove(store);
-    }
-
     public static void main(String[] args) throws Exception {
         INameserver component = ComponentFactory.createNameserver(args[0], System.in, System.out);
         component.run();
