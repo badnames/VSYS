7d450c12bae49620d6a57b33e82a828f5be8a185
Moritz Schalk <e52005233@student.tuwien.ac.at>
2023-01-05 14:40:43 +0100

    Add thread safety to logger.

--- src/main/java/dslab/nameserver/Logger.java
+++ src/main/java/dslab/nameserver/Logger.java
@@ -4,20 +4,27 @@ import java.io.PrintStream;
 import java.time.LocalDateTime;
 
 public class Logger {
+    private static final Object streamLock = new Object();
     public static PrintStream logStream;
 
     public static void log(String message) {
         var currentTime = LocalDateTime.now();
-        logStream.println("[" + currentTime.getYear()
+        String output = "[" + currentTime.getYear()
                 + "-" + currentTime.getMonthValue()
                 + "-" + currentTime.getDayOfMonth()
                 + " " + currentTime.getHour()
                 + ":" + currentTime.getMinute()
                 + ":" + currentTime.getSecond()
-                + "] " + message);
+                + "] " + message;
+
+        synchronized (streamLock) {
+            logStream.println(output);
+        }
     }
 
     public static void setLogStream(PrintStream stream) {
-        logStream = stream;
+        synchronized (streamLock) {
+            logStream = stream;
+        }
     }
 }
